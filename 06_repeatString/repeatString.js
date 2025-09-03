8b028d636259d991ec22c4ffb14500d8cc9d7a84
crux-dev-039 <223843348+Crux0724@users.noreply.github.com>
2025-09-03 16:52:10 +0800

    Finish exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let chosenString = "";
+    for (i = 0; i < num; i++) {
+        chosenString += string;
+    }
+    if (num < 0) {
+        return 'ERROR'
+    }
+    return chosenString;
 };
 
 // Do not edit below this line
