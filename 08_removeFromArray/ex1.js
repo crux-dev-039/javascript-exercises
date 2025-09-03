2646d0f63282d1ef7436fa3c722fe6b4ae53225a
crux-dev-039 <223843348+Crux0724@users.noreply.github.com>
2025-09-03 22:51:01 +0800

    Finish exercise 08_removeFromArray

--- 08_removeFromArray/ex1.js
+++ 08_removeFromArray/ex1.js
@@ -1,16 +1,17 @@
 const removeFromArray = function(array) {
     const args = Array.from(arguments).splice(1);
+    console.log(args);
+    console.log(arguments);
     for (l = 0; l < args.length; l++) {
         for (i = 0; i < array.length; i++) {
             if (array.indexOf(args[l]) >= 0) {
                 array.splice(array.indexOf(args[l]), 1);
             }
+            console.log(array);
         }
     }
+    // console.log(item.length)
     return array;
 }
 
-console.log(removeFromArray([1, 2, 2, 4], 2));
-
-// Do not edit below this line
-module.exports = removeFromArray;
+removeFromArray([1, 2, 2, 4], 2);
