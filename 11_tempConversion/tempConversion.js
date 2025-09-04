285b62bb202d3493d07a18f94aa8961f4089d062
crux-dev-039 <223843348+Crux0724@users.noreply.github.com>
2025-09-04 14:28:59 +0800

    Finish exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    const conversion = ((temp - 32) * (5/9));
+    const rounding = Math.round(conversion * 10) / 10;
+    return rounding
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    const conversion = (temp * (9/5) + 32)
+    const rounding = Math.round(conversion * 10) / 10;
+    return rounding
 };
 
 // Do not edit below this line
